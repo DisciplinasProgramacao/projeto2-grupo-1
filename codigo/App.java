e58244d54e5767a1256ac2cdde306dd4ae1648ea
Henrique Almeida <hcarvalhoalmeida7@gmail.com>
2023-03-22 21:51:25 -0300

    Código do caram

---
+++ codigo/App.java
@@ -0,0 +1,27 @@
+public class App {
+
+    static void carregaGrafos() {
+
+    }
+
+    static void salvarGrafos() {
+
+    }
+
+    static void geraGrafoCompleto() {
+
+    }
+
+    static void geraSubgrafo() {
+
+    }
+
+    static void novoGrafo() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
