8c298fedc0becd7b869f59b70f9f37a9e6faf0fa
dolabelag <dolabelag@gmail.com>
2023-04-03 09:10:12 -0300

    Update Diagrama e Grafo completo #1

--- codigo/GrafoCompleto.java
+++ codigo/GrafoCompleto.java
@@ -1,3 +1,6 @@
+/**
+ * Grafo completo
+ */
 public class GrafoCompleto extends Grafo {
 
     /**
@@ -14,31 +17,6 @@ public class GrafoCompleto extends Grafo {
         for (int i = 0; i < ordem; i++)
             for (int j = i + 1; j < ordem; j++)
                 this.addAresta(i, j, 0);
-    }
-
-    /**
-     * Retorna true se o grafo for completo e false caso contrário
-     * 
-     * @return TRUE se o grafo for completo, FALSE caso contrário
-     */
-    @Override
-    public boolean completo() {
-        return true;
-    }
-
-    /**
-     * Converte para grafo mutável
-     * 
-     * @return Grafo mutável
-     */
-    public GrafoNãoDirecionado toGrafo() {
-        GrafoNãoDirecionado g = new GrafoNãoDirecionado(this.nome);
-        for (int i = 0; i < this.vertices.size(); i++)
-            g.addVertice(i);
-        for (int i = 0; i < this.vertices.size(); i++)
-            for (int j = i + 1; j < this.vertices.size(); j++)
-                g.addAresta(i, j, 0);
-        return g;
     }
 
 }
